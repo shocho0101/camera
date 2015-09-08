669c6d0d051cd59b90552c3605436cb078130a70
張翔 <sho@choushou-no-MacBook-Air.local>
2015-09-08 20:27:06 +0900

    first commit

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var camera:Bool! = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +23,66 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func action(){
+        let actionsheet: UIAlertController = UIAlertController(title: "title", message: "message", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let cancel: UIAlertAction = UIAlertAction(title: "cancel", style: UIAlertActionStyle.Cancel) { (UIAlertAction) -> Void in
+            println("cancel")
+        }
+        
+        let picture: UIAlertAction = UIAlertAction(title: "カメラで撮影", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+            self.takepicture()
+            println("picture")
+        }
+        
+        let album: UIAlertAction = UIAlertAction(title: "アルバムから選択", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+            self.fromalbum()
+            println("album")
+        }
+        
+        actionsheet.addAction(cancel)
+        actionsheet.addAction(album)
+        actionsheet.addAction(picture)
+        
+        self.presentViewController(actionsheet, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    func takepicture(){
+        let imagePickerController: UIImagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.Camera
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+        
+        camera = true
+    }
+    
+    func fromalbum(){
+        let imagePickerController: UIImagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+        
+        camera = false
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imageView.image = image
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+        if camera == true{
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
+    }
+    
+    
 
 
 }
